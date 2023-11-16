745d4a79d74507a4a885cdfae1ee86062017b6af
Rama-Selva-Thangam <ramaselvathangam1999@gmail.com>
2023-11-16 18:03:01 +0530

    Spiral Triangle Matrix Code Fixed

--- src/com/ramaselvathangamm/evaluation1/NumberPattern.java
+++ src/com/ramaselvathangamm/evaluation1/NumberPattern.java
@@ -7,8 +7,8 @@ public class NumberPattern {
 		int[][] arr = new int[rows][rows];
 		int top = 0;
 		int bottom = rows - 1;
-		int left = 0; 
-		int right = rows - 1; 
+		int left = 0;
+		int right = rows - 1;
 		int num = 1;
 		while (top < bottom || left < right) {
 			for (int i = top; i <= bottom; i++) {
@@ -39,11 +39,44 @@ public class NumberPattern {
 		}
 	}
 
+	public static void printTriangleSpiral(int n) { // New Code
+		int[][] matrix = new int[n][n];
+		int number = 1;
+		int left = 0, right = n - 1, top = 0, bottom = n - 1;
+		while (left <= right && top <= bottom) {
+			for (int i = top; i <= bottom; i++) {
+				matrix[i][i + top] = number++;
+			}
+			bottom--;
+			left++;
+			for (int i = bottom; i >= top; i--) {
+				matrix[i][right] = number++;
+			}
+			right--;
+			bottom--;
+			for (int i = right; i >= left; i--) {
+				matrix[top][i] = number++;
+			}
+			top++;
+			left++;
+		}
+		for (int[] k : matrix) {
+			for (int l : k) {
+				if (l == 0) {
+					System.out.printf("   ");
+				} else {
+					System.out.printf("%2d ", l);
+				}
+			}
+			System.out.println();
+		}
+	}
+
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the Number of Rows : ");
 		int n = scan.nextInt();
-		spiralPattern(n);
+		printTriangleSpiral(n);
 		scan.close();
 	}
 }

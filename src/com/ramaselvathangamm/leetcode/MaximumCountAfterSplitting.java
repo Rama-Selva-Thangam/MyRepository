ba549a681b553c9df90abfd0d25b4cdd0577d495
pcconfig <pcconfig@172.17.60.7>
2023-12-22 18:28:30 +0530

    Dec_22

--- src/com/ramaselvathangamm/leetcode/MaximumCountAfterSplitting.java
+++ src/com/ramaselvathangamm/leetcode/MaximumCountAfterSplitting.java
@@ -1,9 +1,34 @@
 package com.ramaselvathangamm.leetcode;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class MaximumCountAfterSplitting {
+	public static int maxScore(String s) {
+		int onesCountRight = 0, zeroesCountLeft = 0, maxScore = 0;
+		for (char ch : s.toCharArray()) {
+			if (ch == '1') {
+				onesCountRight++;
+			}
+		}
+		for (int i = 0; i < s.length() - 1; i++) {
+			if (s.charAt(i) == '0') {
+				zeroesCountLeft++;
+			} else {
+				onesCountRight--;
+			}
+
+			maxScore = Math.max(maxScore, zeroesCountLeft + onesCountRight);
+		}
+		return maxScore;
+	}
 
 	public static void main(String[] args) {
-
+		Scanner scan = new Scanner(System.in);
+		System.out.print("Enter the  String : ");
+		String digits = scan.nextLine();
+		int result = maxScore(digits);
+		System.out.println(result);
 	}
 
 }

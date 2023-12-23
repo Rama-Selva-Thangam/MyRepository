9e86ccffa3ab762b808c8d262abfa6c039ffdf99
pcconfig <pcconfig@172.17.60.7>
2023-12-23 12:44:48 +0530

    Question_3_Solved

--- src/com/ramaselvathangamm/evaluation3/DungeonGame.java
+++ src/com/ramaselvathangamm/evaluation3/DungeonGame.java
@@ -1,5 +1,8 @@
 package com.ramaselvathangamm.evaluation3;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class DungeonGame {
 	public static int minSteps(char[][] plane, char adventure, char gold) {
 		int rows = plane.length;
@@ -33,6 +36,52 @@ public class DungeonGame {
 		return adventureStep;
 	}
 
+	public static List<List<Integer>> minStepPath(char[][] plane, char adventure, char gold, char monster) {
+		int steps = minStepsWithMonster(plane, adventure, gold, monster);
+		if (steps < 0) {
+			return null;
+		}
+		List<List<Integer>> result = new ArrayList<List<Integer>>();
+		List<Integer> path = new ArrayList<Integer>();
+		int adventureX = -1, adventureY = -1;
+		int goldX = -1, goldY = -1;
+		for (int i = 0; i < plane.length; i++) {
+			for (int j = 0; j < plane[i].length; j++) {
+				if (plane[i][j] == adventure) {
+					adventureX = i + 1;
+					adventureY = j + 1;
+				} else if (plane[i][j] == gold) {
+					goldX = i + 1;
+					goldY = j + 1;
+				}
+			}
+		}
+
+		if (adventureX == -1) {
+			return null;
+		}
+		path.add(adventureX);
+		path.add(adventureY);
+		result.add(new ArrayList<Integer>(path));
+		while (adventureX != goldX || adventureY != goldY) {
+			if (adventureX < goldX) {
+				adventureX++;
+			} else if (adventureX > goldX) {
+				adventureX--;
+			} else if (adventureY < goldY) {
+				adventureY++;
+			} else if (adventureY > goldY) {
+				adventureY--;
+			}
+			path.clear();
+			path.add(adventureX);
+			path.add(adventureY);
+			result.add(new ArrayList<Integer>(path));
+		}
+
+		return result;
+	}
+
 	public static void main(String[] args) {
 		char[][] plane = new char[5][4];
 		plane[4][0] = 'A';
@@ -44,6 +93,18 @@ public class DungeonGame {
 		plane[3][2] = 'G';
 		plane[2][0] = 'M';
 		int output2 = minStepsWithMonster(plane, 'A', 'G', 'M');
-		System.out.println(output2 > 0 ? ("Minimum Number of Steps : " + output2) : ("No Possible Solution"));
+		System.out.println(output2 >= 0 ? ("Minimum Number of Steps : " + output2) : ("No Possible Solution"));
+		List<List<Integer>> output3 = minStepPath(plane, 'A', 'G', 'M');
+		if (output3 != null) {
+			System.out.println("Minimum Number of Steps : " + (output3.size() - 1));
+			int i = 0;
+			for (List<Integer> li : output3) {
+				i++;
+				System.out.print(li);
+				if (i < output3.size()) {
+					System.out.print("-->");
+				}
+			}
+		}
 	}
 }
